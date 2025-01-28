1271451fc00356b2e6054d7d199595fd56658c83
chriskira03 <christian.valdivia03@gmail.com>
2025-01-28 09:46:47 -0500

    Organizar estructura del proyecto

---
+++ website/src/app/prueba/Prueba.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Prueba = () => {
+  return (
+    <>
+    <div className='my-52' >
+    <div className='text-red-500 text-3xl font-extrabold text-center  items-center justify-center m-auto' >
+            En esta ruta se encuentan los componentes para realizar prueba de componentes y estilos de la aplicacion
+        </div>
+        <div className='text-2xl text-blue-600 items-center justify-center flex' >
+            No realizar commit en esta ruta
+        </div>
+    </div>
+
+    </>
+
+  )
+}
---
+++ website/src/app/prueba/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Prueba } from './Prueba'
+
+const page = () => {
+  return (
+    <Prueba />
+  )
+}
+
+export default page
