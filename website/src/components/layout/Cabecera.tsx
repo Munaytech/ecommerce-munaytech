fc1e1b307560d83229c0bec0b234c80534b61ffe
Munaytech <munaytech@hotmail.com>
2025-04-30 11:16:29 -0500

    Update Cabecera.tsx

--- website/src/components/layout/Cabecera.tsx
+++ website/src/components/layout/Cabecera.tsx
@@ -7,7 +7,7 @@ export const Cabecera = () => {
       <div className="w-[1500px] m-auto text-white py-1 flex justify-between items-center  ">
         <div className="flex items-center justify-center text-sm gap-x-4">
           <p className="bg-primary rounded-2xl py-1 px-3  font-semibold">HOT</p>
-          <p>Free Express Shipping</p>
+          <p>Free Express MUNAY</p>
         </div>
         <div className="flex items-center justify-center gap-x-3" >
           <Twiter />
